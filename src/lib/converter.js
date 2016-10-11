5e7f351024c15505e4d3d1bf70f688834151d751
Mangi Du <duxiaoxue93@gmail.com>
2016-10-11 20:06:47 +0800

    develop:增加一个换行,防止apib编译报错

--- src/lib/converter.js
+++ src/lib/converter.js
@@ -4,7 +4,7 @@ const SPACE_GAP = ' '.repeat(4)
 const HEAD_PREFIX = '#'
 const LIST_PREFIX = '+'
 const LINE_BREAK = '\n'
-const DOUBLE_LINE_BREAK = '\n'.repeat(2)
+const MULTI_LINE_BREAK = '\n'.repeat(3)
 
 const COLOR = {
   yellow: '#dec24a',
@@ -25,7 +25,7 @@ function type (text, hasColor, color='#000') {
 let converter = {}
 
 converter.group = function (inputObj, option={}) {
-  let result = `${HEAD_PREFIX} Group ${inputObj.name}${DOUBLE_LINE_BREAK}`
+  let result = `${HEAD_PREFIX} Group ${inputObj.name}${MULTI_LINE_BREAK}`
   return type(result, option.hasColor, COLOR.green)
 }
 
@@ -38,11 +38,11 @@ converter.basic = function (inputObj, option={}) {
   }
   let part1 = `${HEAD_PREFIX} ${inputObj.keyword}`
   let part2 = `${inputObj.method} ${inputObj.path}${keys}`
-  return `${type(part1, option.hasColor, COLOR.green)} [${type(part2, option.hasColor, COLOR.yellow)}]${DOUBLE_LINE_BREAK}`
+  return `${type(part1, option.hasColor, COLOR.green)} [${type(part2, option.hasColor, COLOR.yellow)}]${MULTI_LINE_BREAK}`
 }
 
 converter.descrip = function (inputObj, option={}) {
-  return `${inputObj.descrip}${DOUBLE_LINE_BREAK}`
+  return `${inputObj.descrip}${MULTI_LINE_BREAK}`
 }
 
 converter.params = function (inputArr, option={}) {
@@ -53,7 +53,7 @@ converter.params = function (inputArr, option={}) {
   for (let param of inputArr) {
     params += `${SPACE_GAP}${type(LIST_PREFIX, option.hasColor, COLOR.red)} ${param.keyword}: \`${type('example data', option.hasColor, COLOR.green)}\` (${type(param.paraType, option.hasColor, COLOR.yellow)}, ${type(param.requirement, option.hasColor, COLOR.blue)}) - ${param.description}${LINE_BREAK}`
   }
-  return `${type(LIST_PREFIX, option.hasColor, COLOR.red)} Parameters${LINE_BREAK}${params}${DOUBLE_LINE_BREAK}`
+  return `${type(LIST_PREFIX, option.hasColor, COLOR.red)} Parameters${LINE_BREAK}${params}${MULTI_LINE_BREAK}`
 }
 
 converter.request = function (inputArr, option={}) {
@@ -64,7 +64,7 @@ converter.request = function (inputArr, option={}) {
   for (let req of inputArr) {
     reqs += `${SPACE_GAP.repeat(2)}${type(LIST_PREFIX, option.hasColor, COLOR.red)} ${req.keyword}: \`${type('example data', option.hasColor, COLOR.green)}\` (${type(req.paraType, option.hasColor, COLOR.yellow)}, ${type(req.requirement, option.hasColor, COLOR.blue)}) - ${req.description}${LINE_BREAK}`
   }
-  return `${type(LIST_PREFIX, option.hasColor, COLOR.red)} Request (application/json;charset=utf-8)${LINE_BREAK}${SPACE_GAP}${type(LIST_PREFIX, option.hasColor, COLOR.red)} Attributes${LINE_BREAK}${reqs}${DOUBLE_LINE_BREAK}`
+  return `${type(LIST_PREFIX, option.hasColor, COLOR.red)} Request (application/json;charset=utf-8)${LINE_BREAK}${SPACE_GAP}${type(LIST_PREFIX, option.hasColor, COLOR.red)} Attributes${LINE_BREAK}${reqs}${MULTI_LINE_BREAK}`
 }
 
 converter.response = function (inputObj, option={}) {
@@ -76,7 +76,7 @@ converter.response = function (inputObj, option={}) {
   for (let res of inputObj.arr || []) {
     ress += `${SPACE_GAP.repeat(2)}${type(LIST_PREFIX, option.hasColor, COLOR.red)} ${res.keyword}: \`${type('example data', option.hasColor, COLOR.green)}\` (${type(res.paraType, option.hasColor, COLOR.yellow)}, ${type(res.requirement, option.hasColor, COLOR.blue)}) - ${res.description}${LINE_BREAK}`
   }
-  return `${type(LIST_PREFIX, option.hasColor, COLOR.red)} Response ${type(inputObj.code, option.hasColor, COLOR.yellow)} ${type('(application/json;charset=utf-8)', option.hasColor, COLOR.red)}${LINE_BREAK}${SPACE_GAP}${type(LIST_PREFIX, option.hasColor, COLOR.red)} Attributes${LINE_BREAK}${ress}${DOUBLE_LINE_BREAK}`
+  return `${type(LIST_PREFIX, option.hasColor, COLOR.red)} Response ${type(inputObj.code, option.hasColor, COLOR.yellow)} ${type('(application/json;charset=utf-8)', option.hasColor, COLOR.red)}${LINE_BREAK}${SPACE_GAP}${type(LIST_PREFIX, option.hasColor, COLOR.red)} Attributes${LINE_BREAK}${ress}${MULTI_LINE_BREAK}`
 }
 
 export default converter
