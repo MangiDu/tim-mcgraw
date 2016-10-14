34e8bd59e99ab156ebc17617c8175a96302928b9
Mangi Du <duxiaoxue93@gmail.com>
2016-10-14 10:09:00 +0800

    develop:select-input的上下键操作

--- src/components/common/SelectInput.vue
+++ src/components/common/SelectInput.vue
@@ -1,9 +1,9 @@
 <template lang="html">
   <div class="select-input">
-    <input type="text" v-model="item" @focus="inputFocus = true" @blur="inputFocus = false" @keyup.enter="checkToSet()">
+    <input type="text" v-model="item" @focus="inputFocus = true" @blur="inputFocus = false" @keyup.enter="checkToSet()" @keyup.up="up" @keyup.down="down">
     <ul class="list" v-show="showList" @mouseover="listMouseover = true" @mouseout="listMouseover = false">
       <!-- <li class="item" v-for="listItem in list | filterBy item" @click="set(listItem)">{{ listItem }}</li> -->
-      <li class="item" v-for="listItem in list" @click="set(listItem)">{{ listItem }}</li>
+      <li class="item" v-for="listItem in list" @click="set($index, true)" :class="{'active': $index === index}">{{ listItem }}</li>
     </ul>
   </div>
 </template>
@@ -22,7 +22,8 @@ export default {
   data () {
     return {
       listMouseover: false,
-      inputFocus: false
+      inputFocus: false,
+      index: -1
     }
   },
   computed: {
@@ -35,14 +36,35 @@ export default {
       this.inputFocus = false
       this.listMouseover = false
     },
-    set (item) {
-      this.item = item
-      this.reset()
+    set (index, reset = false) {
+      this.index = index || this.index
+      this.item = this.list[this.index]
+      if (reset) {
+        this.reset()
+      }
     },
     checkToSet (list) {
       // TODO:这个方法想做的事情是回车后自动补全
       // vue的过滤器仅在模板中才起作用,2.0有在讨论这个问题
       // https://github.com/vuejs/vue/issues/2756
+    },
+    up () {
+      if (this.index > 0) {
+        this.index--
+      }
+      else {
+        this.index = this.list.length - 1
+      }
+      this.set()
+    },
+    down () {
+      if (this.index < this.list.length - 1) {
+        this.index++
+      }
+      else {
+        this.index = 0
+      }
+      this.set()
     }
   }
 }
